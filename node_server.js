a487574481992c692dd851e3c7217921fa3784cf
Haridarshan HS <hsdars@gmail.com>
2018-12-22 15:30:44 +0530

    update due to some security issue
    as the id and token werent supposed to be publicly available

--- node_server.js
+++ node_server.js
@@ -9,19 +9,19 @@ var app = express();
 app.use(cors());
 var Twitter = require('twitter');
 var client = new Twitter({
-    consumer_key: 'LmNp3JwAQZnuBr4SQFaM7UZG3',
-    consumer_secret: 'Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD',
-    access_token_key: '151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc',
-    access_token_secret: 'czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw'
+    consumer_key: 'consumer_key',
+    consumer_secret: 'consumer_secret',
+    access_token_key: 'access_token_key',
+    access_token_secret: 'access_token_secret'
 });
 var watson = require('watson-developer-cloud');
-var MyClient = require('../lib/idol-client.js')('f3129194-4f03-4419-80c2-f3aa041baf9a');
+var MyClient = require('../lib/idol-client.js')('id');
 MyClient.Q.longStackSupport = true;
 var AlchemyAPI = require('alchemy-api');
-var alchemy = new AlchemyAPI('7b6bf4773c39c9e271f6bd999fea5df5179a6dad');
-var apiKey = "DAKa8696003222b4812850342de17d0e267"; // Get from kandy.io 
-var userId = "user1"; // Get from kandy.io 
-var password = "1euminciduntconse1"; // Get from kandy.io 
+var alchemy = new AlchemyAPI('id');
+var apiKey = "apiKey"; // Get from kandy.io 
+var userId = "userId"; // Get from kandy.io 
+var password = "password"; // Get from kandy.io 
 var Kandy = require("kandy");
 var kandy = new Kandy(apiKey);
 var userAccessToken;
@@ -33,12 +33,12 @@ kandy.getUserAccessToken(userId, password, function(data, response) {
 
 kandy = new Kandy();
 var sendgrid = require('sendgrid')('hsdars', 'Darshanhs90-');
-var accountSid = 'AC07275e4294f1b0d42623c3ec9559911e';
-var authToken = '650d049a9bd99323fb899ce4b9e84fcc';
+var accountSid = 'id';
+var authToken = 'token';
 var clientTwilio = require('twilio')(accountSid, authToken);
 var speech_to_text = watson.speech_to_text({
-    username: '1a4e2a43-2a65-4e28-b9bc-2947c6a48e47',
-    password: 'WNMkUbFzLf6c',
+    username: 'username',
+    password: 'password',
     version: 'v1'
 });
 //company ip declaration
@@ -62,8 +62,8 @@ app.get('/sendMail', function(reqst, rspns) {
     var textMail;
     //get from "name","to email address","company name"
     sendgrid.send({
-        to: 'hsdars@gmail.com',
-        from: 'hsdars@gmail.com',
+        to: 'id',
+        from: 'id',
         subject: 'Hello World',
         text: 'My first email through SendGrid.'
     }, function(err, json) {
@@ -78,8 +78,8 @@ app.get('/sendMail', function(reqst, rspns) {
 //send sms from employer to student
 app.get('/sendSms', function(reqst, respns) {
   console.log(reqst.query);
-    var from = "+14694164117"; // Optional 
-    var to = "+14697672278"; // Required ,change it
+    var from = "+num"; // Optional 
+    var to = "+othnum"; // Required ,change it
     var text = "Hello from Kandy"; // Required ,change it
 
 
@@ -128,7 +128,7 @@ app.use('/twitterCompanySentiment', function(reqst, respns) {
 app.use('/linkedjobsfeed', function(req, res) {
     var num = (req.url.substring(9));
     var ip = cid[num];
-    https.get('https://api.linkedin.com/v1/companies/' + ip + '/updates?oauth2_access_token=AQUxHxrW_VKxyQoipBT3-37W7cj3NJhF0LLmGARZvGX2hziIY9gdy8HEdaZHTGLPuo8T8mHdD0X64HEZIQeSL-6mj8hMLAGPH2IuZAwi8HF_6sFe7sM79r0q1wxyxZpCXcT_M8gicNIieekmGegFpHp8H-UqjPnTLwWNKUd1HI4taXVbbdg&format=json',
+    https.get('https://api.linkedin.com/v1/companies/' + ip + '/updates?oauth2_access_token=oauth2_access_token&format=json',
         function(response) {
             var body = '';
             response.on('data', function(d) {
@@ -153,7 +153,7 @@ app.get('/personalityinsights', function(reqst, respns) {
             url = url.replace(":", "%3A");
             //console.log(url);
             //https.get('https://api.idolondemand.com/1/api/async/recognizespeech/v1?url=http%3A%2F%2Fwww.talkenglish.com%2FAudioTE%2FL21%2Fsentence%2FL21S5.mp3&interval=0&apikey=f3129194-4f03-4419-80c2-f3aa041baf9a',function(res){
-            https.get('https://api.idolondemand.com/1/api/async/recognizespeech/v1?url=' + url + '&apikey=f3129194-4f03-4419-80c2-f3aa041baf9a', function(res) {
+            https.get('https://api.idolondemand.com/1/api/async/recognizespeech/v1?url=' + url + '&apikey=apikey', function(res) {
 
                         var data = '';
                         res.on('data', function(d) {
@@ -164,7 +164,7 @@ app.get('/personalityinsights', function(reqst, respns) {
                                     data = JSON.parse(data);
                                     //console.log(data);
                                     jobID = data.jobID;
-                                    https.get('https://api.idolondemand.com/1/job/result/' + jobID + '?apikey=f3129194-4f03-4419-80c2-f3aa041baf9a', function(res) {
+                                    https.get('https://api.idolondemand.com/1/job/result/' + jobID + '?apikey=apikey', function(res) {
 
                                         var dt1 = '';
                                         res.on('data', function(dt) {
@@ -176,8 +176,8 @@ app.get('/personalityinsights', function(reqst, respns) {
 
 
                                             var personality_insights = watson.personality_insights({
-                                                "username": "aa358f77-75ab-4560-82ed-bbf4aa1c1b4b",
-                                                "password": "Mo0l98NbJP01",
+                                                "username": "username",
+                                                "password": "password",
                                                 version: 'v2'
                                             });
 
@@ -199,9 +199,9 @@ app.get('/personalityinsights', function(reqst, respns) {
                                     app.get('/schedulecallnotification', function(reqst, respns) {
                                       console.log(reqst.query);
                                         clientTwilio.calls.create({
-                                            to: "+14697672278",
-                                            from: "+14694164117",
-                                            url: "https://www.dropbox.com/s/3nsmfduffri1lg5/twilio.xml",
+                                            to: "+num",
+                                            from: "+othnum",
+                                            url: "fileurl",
                                             method: "GET",
                                             fallbackMethod: "GET",
                                             statusCallbackMethod: "GET",
@@ -217,7 +217,7 @@ app.get('/personalityinsights', function(reqst, respns) {
                                     //get company info
                                     app.get('/companyInfo', function(reqst, respns) {
                                         //company website
-                                        https.get('https://api.fullcontact.com/v2/company/lookup.json?domain=google.com&apiKey=f6e2b2695278badc',
+                                        https.get('https://api.fullcontact.com/v2/company/lookup.json?domain=google.com&apiKey=apiKey',
                                             function(response) {
                                                 var body = '';
                                                 response.on('data', function(d) {
@@ -238,7 +238,7 @@ app.get('/personalityinsights', function(reqst, respns) {
                                     //get student info
                                     app.get('/personInfo', function(reqst, respns) {
                                         //person email id  
-                                        https.get('https://api.fullcontact.com/v2/person.json?email=bart@fullcontact.com&apiKey=f6e2b2695278badc',
+                                        https.get('https://api.fullcontact.com/v2/person.json?email=bart@fullcontact.com&apiKey=apiKey',
                                             function(response) {
                                                 var body = '';
                                                 response.on('data', function(d) {
